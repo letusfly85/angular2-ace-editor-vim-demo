84a153e8750a77e2ef712201c8f70ca3545eef0e
letusfly85 <letusfly95@gmail.com>
2018-05-08 16:13:00 +0900

    feat: add angular2 ace snippets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import 'brace/theme/github';
+import 'brace/mode/python';
+import 'brace/keybinding/vim';
+declare let ace: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit, AfterViewInit {
+  content: string;
+  options: any;
+  @ViewChild('highlight') highlight;
+  @ViewChild('editorInfinity') editorInfinity;
+  @ViewChild('firstEditor') firstEditor;
+
+  ngOnInit() {
+    this.content = '';
+    this.options = {maxLines: 1000, printMargin: false};
+  }
+
+  onRuleChange(e) {
+    console.log(e);
+  }
+
+  ngAfterViewInit() {
+    const editor = this.firstEditor.getEditor();
+
+    editor.session.setOption('useWorker', true);
+    editor.setOption('fontSize', '16pt');
+    ace.config.loadModule('ace/keyboard/vim', module => {
+      const VimApi = module.CodeMirror.Vim;
+      VimApi.defineEx('write', 'w', (cm, input) => {
+        console.log('write');
+      });
+    });
+    editor.setKeyboardHandler('ace/keyboard/vim');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AceEditorModule } from 'ng2-ace-editor';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AceEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
